0fe9b4610ed20eb5aa7dae66ee231c7a3cbb5d98
Igor Kompanets <kombainer86@gmail.com>
2022-10-15 19:08:48 +0300

    task1 complete

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать массив из 8 элементов и заполнить его нулями и единицами и вывести его на экран
+
+int size = 8;
+int [] arr = new int [size];
+
+for (int i = 0; i < size; i++)
+    arr[i] = new Random().Next(0,2);
+
+Console.WriteLine("Ваш массив: ");
+for (int i = 0; i < size; i++)
+    Console.Write($"{arr[i]} ");
