d6e32d52318451cf770812424ad5a26ea4696879
Igor Kompanets <kombainer86@gmail.com>
2022-10-15 20:57:56 +0300

    task6 complete

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.WriteLine("Введите количество элементов массива");
+int size = int.Parse(Console.ReadLine());
+int [] arr = new int [size];
+
+for (int i = 0; i < size; i++)
+    arr[i] = new Random().Next(0,100);
+
+Console.WriteLine("Ваш массив: ");
+for (int i = 0; i < size; i++)
+    Console.Write($"{arr[i]} ");
+int sum = 0;
+for (int i = 0; i < size; i = i + 2) //Считаем элемент с индексом 0 стоит на нечетной позиции (для пользователя эта позиция равна 1)
+    sum = sum + arr[i];
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел массива, стоящих на нечетной позиции равна: {sum}");
---
+++ Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+Console.WriteLine("Введите количество элементов массива");
+int size = int.Parse(Console.ReadLine());
+int [] arr = new int [size];
+
+for (int i = 0; i < size; i++)
+    arr[i] = new Random().Next(0,100);
+
+Console.WriteLine("Ваш массив: ");
+for (int i = 0; i < size; i++)
+    Console.Write($"{arr[i]} ");
+int min = 0;
+int max = size - 1;
+int count = 0;
+int multi = 0;
+while (min < max)
+{
+    multi=arr[min] * arr[max];
+    count++;
+    min++;
+    max--;
+    Console.WriteLine();
+    Console.WriteLine($"Произведение чисел {count} пары равно {multi}");
+}
