e6333adce90254fc8d133a316c0956075465b3f5
Igor Kompanets <kombainer86@gmail.com>
2022-10-15 19:49:06 +0300

    task4 complete

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int size = 123;
+int [] arr = new int [size];
+
+for (int i = 0; i < size; i++)
+    arr[i] = new Random().Next(0,1000);
+
+Console.WriteLine("Ваш массив: ");
+for (int i = 0; i < size; i++)
+    Console.Write($"{arr[i]} ");
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    if(arr[i] >= 10 && arr[i] <=99)
+        count++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чиселиз отрезка [10,99] в массиве равно: {count}");
